276cde3f380e64bffc8faa8a899a070c4aa5d73f
Extreme Team <extreme.team@wds.co>
2014-10-01 14:15:33 +0100

    PR Cleaned up unused code

--- src/main/java/co/wds/testingtools/testinjection/TestInjectionUtils.java
+++ src/main/java/co/wds/testingtools/testinjection/TestInjectionUtils.java
@@ -1,18 +1,13 @@
 package co.wds.testingtools.testinjection;
 
 import java.lang.reflect.Field;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Created by extreme on 05/09/14.
  */
 public class TestInjectionUtils {
-
-    private final static Logger LOGGER = Logger.getLogger(TestInjectionUtils.class.getName());
-
     public static void injectPrivateMember(Object destination, String destinationField, Object objectToInject) throws NoSuchFieldException, IllegalAccessException {
-        Class clazz = destination.getClass();
+        Class<?> clazz = destination.getClass();
         Field fieldToSet = clazz.getDeclaredField(destinationField);
         fieldToSet.setAccessible(true);
         fieldToSet.set(destination, objectToInject);
