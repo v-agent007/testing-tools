3104111bc3dcf6b0854a1597e43c3c8f21e0fa8d
Extreme Team (fozzie) <extreme.team@wds.co>
2014-12-30 09:47:15 +0000

    MD/BH [SD-266] Added functionality to get requests for given path

--- src/main/java/co/wds/testingtools/annotations/MapperServletAnnotations.java
+++ src/main/java/co/wds/testingtools/annotations/MapperServletAnnotations.java
@@ -4,6 +4,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.List;
 
 import org.eclipse.jetty.servlet.ServletHolder;
 
@@ -11,34 +12,46 @@ import co.wds.testingtools.annotations.mapperservlet.AnnotationMapperServlet;
 import co.wds.testingtools.annotations.mapperservlet.Request;
 import co.wds.testingtools.annotations.mapperservlet.TestingServer;
 
+import com.google.common.base.Splitter;
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+
 public class MapperServletAnnotations {
-	
+
 	private static TestingServer server;
 	private static AnnotationMapperServlet mapperServlet;
-	
+
 	@Retention(RetentionPolicy.RUNTIME)
 	@Target(ElementType.TYPE)
 	public @interface TestServlet {
 		int port() default 80;
+
 		String contentType() default "application/json";
+
 		boolean requiresAuthentication() default false;
+
 		String userName() default "";
+
 		String password() default "";
 	}
-	
+
 	@Retention(RetentionPolicy.RUNTIME)
 	@Target(ElementType.TYPE)
 	public @interface RespondTo {
 		ResponseData[] value();
 	}
-	
+
 	@Retention(RetentionPolicy.RUNTIME)
 	@Target(ElementType.TYPE)
 	public @interface ResponseData {
 		String url() default "/";
+
 		String resourceFile();
+
 		String contentType() default "";
+
 		int status() default 200;
+
 		boolean ignoreParams() default false;
 	}
 
@@ -49,7 +62,7 @@ public class MapperServletAnnotations {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void stopMapperServlet() {
 		try {
 			if (server != null) {
@@ -60,39 +73,52 @@ public class MapperServletAnnotations {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static Request mostRecentRequest() {
 		return mapperServlet.getRequests().get(mapperServlet.getRequests().size() - 1);
 	}
-	
+
+	public static List<Request> getRequests(String forPath) {
+		List<Request> result = Lists.newArrayList();
+		String path;
+		Iterable<String> split;
+		for (Request request : mapperServlet.getRequests()) {
+			split = Splitter.on('?').omitEmptyStrings().split(request.url);
+			path = split.iterator().next();
+			if (!Strings.isNullOrEmpty(path) && path.equals(forPath)) {
+				result.add(request);
+			}
+		}
+		return result;
+	}
+
 	public static void addMapping(String uri, String resourceFile, String contentType, int status, boolean ignoreParams) {
 		if (!uri.startsWith("/")) {
 			uri = "/" + uri;
 		}
-		
+
 		if (mapperServlet != null) {
 			mapperServlet.bindReponse(uri, resourceFile, contentType, status, ignoreParams);
 		}
 	}
 
-	private static void processAnnotations(final Class<? extends Object> testClass,	Object testObject) throws Exception {
+	private static void processAnnotations(final Class<? extends Object> testClass, Object testObject) throws Exception {
 		Class<? extends Object> localTestClass = findAnnotatedSuperClass(testClass);
-		
+
 		if (localTestClass == null) {
 			throw new IllegalStateException("Annotation not found");
 		}
-		
+
 		TestServlet testServlet = localTestClass.getAnnotation(TestServlet.class);
 		createServer(testServlet);
-		
+
 		RespondTo respondTo = localTestClass.getAnnotation(RespondTo.class);
 		addResponsesFrom(respondTo, testServlet);
-		
+
 		startServer();
 	}
 
-	private static Class<? extends Object> findAnnotatedSuperClass(
-			final Class<? extends Object> testClass) {
+	private static Class<? extends Object> findAnnotatedSuperClass(final Class<? extends Object> testClass) {
 		Class<? extends Object> curClass = testClass;
 		while (curClass.getAnnotation(TestServlet.class) == null) {
 			curClass = curClass.getSuperclass();
@@ -100,7 +126,7 @@ public class MapperServletAnnotations {
 				return null;
 			}
 		}
-		
+
 		Class<? extends Object> localTestClass = curClass;
 		return localTestClass;
 	}
@@ -131,7 +157,7 @@ public class MapperServletAnnotations {
 				addMapping(uri, resourceFile, contentType, status, ignoreParams);
 			}
 		}
-		
+
 		ServletHolder holder = new ServletHolder(mapperServlet);
 		server.getHandler().addServlet(holder, "/");
 	}
--- src/test/java/co/wds/testingtools/annotations/mapperservlet/RequestCheckTest.java
+++ src/test/java/co/wds/testingtools/annotations/mapperservlet/RequestCheckTest.java
@@ -1,5 +1,6 @@
 package co.wds.testingtools.annotations.mapperservlet;
 
+import static co.wds.testingtools.annotations.MapperServletAnnotations.getRequests;
 import static co.wds.testingtools.annotations.MapperServletAnnotations.mostRecentRequest;
 import static co.wds.testingtools.annotations.MapperServletAnnotations.startMapperServlet;
 import static co.wds.testingtools.annotations.MapperServletAnnotations.stopMapperServlet;
@@ -12,6 +13,7 @@ import static org.junit.Assert.assertThat;
 import java.io.DataOutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
@@ -23,7 +25,10 @@ import co.wds.testingtools.annotations.MapperServletAnnotations.TestServlet;
 
 @TestServlet(port=54321, contentType="text/plain")
 @RespondTo({
-	@ResponseData(url="fake", resourceFile="fake.json", contentType="application/json")
+	@ResponseData(url="fake", resourceFile="fake.json", contentType="application/json"),
+	@ResponseData(url="samefake", resourceFile="fake.json", contentType="application/json"),
+	@ResponseData(url="fakeignoreparams", resourceFile="fake.json", contentType="application/json", ignoreParams=true),
+	@ResponseData(url="samefakeignoreparams", resourceFile="fake.json", contentType="application/json", ignoreParams=true),
 })
 public class RequestCheckTest {
 	@Before
@@ -62,6 +67,87 @@ public class RequestCheckTest {
 		assertThat(requestObject.url, is("/fake"));
 		assertThat(requestObject.body, is(""));
 		assertThat(requestObject.type, is(Request.RequestType.POST));
+	}
+
+	
+	@Test
+	public void getRequestsByUrlShouldStripParams() throws Exception {
+		URL url = new URL("http://localhost:54321/fakeignoreparams?param1=one&param2=two");
+		HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+		connection.setRequestMethod("GET");
+		assertThat(connection.getResponseCode(), is(200));
+		
+		URL url2 = new URL("http://localhost:54321/samefakeignoreparams?animal=cow");
+		connection = (HttpURLConnection)url2.openConnection();
+		connection.setRequestMethod("GET");
+		assertThat(connection.getResponseCode(), is(200));
+		
+		
+		url = new URL("http://localhost:54321/fakeignoreparams");
+		connection = (HttpURLConnection)url.openConnection();
+		connection.setRequestMethod("GET");
+		assertThat(connection.getResponseCode(), is(200));
+		
+		List<Request> requestsList = getRequests("/fakeignoreparams");
+	
+		assertThat(requestsList, is(not(nullValue())));
+		assertThat(requestsList.size(), is(2));
+				
+		assertThat(requestsList.get(0).url, is("/fakeignoreparams?param1=one&param2=two"));
+		assertThat(requestsList.get(0).body, is(""));
+		assertThat(requestsList.get(0).type, is(Request.RequestType.GET));
+		
+		assertThat(requestsList.get(1).url, is("/fakeignoreparams"));
+		assertThat(requestsList.get(1).body, is(""));
+		assertThat(requestsList.get(1).type, is(Request.RequestType.GET));
+		
+		requestsList = getRequests("/samefakeignoreparams");	
+		assertThat(requestsList, is(not(nullValue())));
+		assertThat(requestsList.size(), is(1));
+				
+		assertThat(requestsList.get(0).url, is("/samefakeignoreparams?animal=cow"));
+		assertThat(requestsList.get(0).body, is(""));
+		assertThat(requestsList.get(0).type, is(Request.RequestType.GET));
+	}
+	
+	@Test
+	public void getRequestsByUrlShouldReturnAllRequests() throws Exception {
+		URL url = new URL("http://localhost:54321/fake");
+		HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+		connection.setRequestMethod("POST");
+		assertThat(connection.getResponseCode(), is(200));
+		
+		URL url2 = new URL("http://localhost:54321/samefake");
+		connection = (HttpURLConnection)url2.openConnection();
+		connection.setRequestMethod("POST");
+		assertThat(connection.getResponseCode(), is(200));
+		
+		
+		url = new URL("http://localhost:54321/fake");
+		connection = (HttpURLConnection)url.openConnection();
+		connection.setRequestMethod("POST");
+		assertThat(connection.getResponseCode(), is(200));
+		
+		List<Request> requestsList = getRequests("/fake");
+	
+		assertThat(requestsList, is(not(nullValue())));
+		assertThat(requestsList.size(), is(2));
+				
+		assertThat(requestsList.get(0).url, is("/fake"));
+		assertThat(requestsList.get(0).body, is(""));
+		assertThat(requestsList.get(0).type, is(Request.RequestType.POST));
+		
+		assertThat(requestsList.get(1).url, is("/fake"));
+		assertThat(requestsList.get(1).body, is(""));
+		assertThat(requestsList.get(1).type, is(Request.RequestType.POST));
+		
+		requestsList = getRequests("/samefake");	
+		assertThat(requestsList, is(not(nullValue())));
+		assertThat(requestsList.size(), is(1));
+				
+		assertThat(requestsList.get(0).url, is("/samefake"));
+		assertThat(requestsList.get(0).body, is(""));
+		assertThat(requestsList.get(0).type, is(Request.RequestType.POST));
 	}
 	
 	@Test
